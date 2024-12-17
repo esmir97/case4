03b211822016ede8b028dbace9502a7a4c0bbf82
theoskogsberg <theo.skogsberg02@gmail.com>
2024-12-17 15:45:02 +0100

    bug fixes

--- assets/index.js
+++ assets/index.js
@@ -1,13 +1,13 @@
-//import { renderStart } from "./components/home/renderHome.js";
+import { renderStart } from "./components/home/renderHome.js";
 // import { renderQuizNav, renderQuizHeader, renderQuizQuestion } from "./components/quiz/renderQuiz.js";
-import { renderWrongAnswer } from "./components/wrongAnswer/renderWrongAnswer.js";
+// import { renderWrongAnswer } from "./components/wrongAnswer/renderWrongAnswer.js";
 
 const wrapper = document.getElementById("wrapper");
 
-//renderStart(wrapper);
+renderStart(wrapper);
 // renderQuizNav(wrapper);
 // renderQuizHeader(wrapper);
 // renderQuizQuestion(wrapper);
 //renderCorrectAnswer(wrapper);
 
-renderWrongAnswer(wrapper);
+// renderWrongAnswer(wrapper);
