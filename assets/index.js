29a83dff9f929d5185859a1d19fc04721d7cb0a5
theoskogsberg <theo.skogsberg02@gmail.com>
2024-12-17 11:37:12 +0100

    Revert "bug fixes"
    This reverts commit 20ec1f0033e38a4f5e9e6088347ebf2d97f07d11.

--- assets/index.js
+++ assets/index.js
@@ -1,9 +1,9 @@
-import { renderStart } from "./components/home/renderHome.js";
-// import { renderQuizNav, renderQuizHeader, renderQuizQuestion } from "./components/quiz/renderQuiz.js";
+// import { renderStart } from "./components/home/renderHome.js";
+import { renderQuizNav, renderQuizHeader, renderQuizQuestion } from "./components/quiz/renderQuiz.js";
 
 const wrapper = document.getElementById("wrapper");
 
-renderStart(wrapper);
-// renderQuizNav(wrapper);
-// renderQuizHeader(wrapper);
-// renderQuizQuestion(wrapper);
+// renderStart(wrapper);
+renderQuizNav(wrapper);
+renderQuizHeader(wrapper);
+renderQuizQuestion(wrapper);
