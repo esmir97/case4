4df2e7964233c654462dc41551a111b709f30821
theoskogsberg <theo.skogsberg02@gmail.com>
2024-12-23 15:20:48 +0100

    Update ending.js

---
+++ assets/components/ending/ending.js
@@ -0,0 +1,14 @@
+
+function renderEnding (parentElement) {
+    let container = document.createElement("div");
+    container.classList.add("container");
+
+    container.innerHTML = `
+        <h2 class= "endingTitle">Want to Play Again?</h2>
+        <h4 class= "h4-bold">Vote for the next quiz to play:</h4>
+        <p class= "voteInfoText">Click to vote!</p>
+
+        
+
+    `;
+};
