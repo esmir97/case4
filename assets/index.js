0fe46919ed77acfff9d626ec281e19adc08995c8
theoskogsberg <theo.skogsberg02@gmail.com>
2024-12-23 16:21:06 +0100

    Update index.js

--- assets/index.js
+++ assets/index.js
@@ -1,13 +1,13 @@
-// import { renderStart } from "./components/home/renderHome.js";
+import { renderStart } from "./components/home/renderHome.js";
 import { establishWebsocket } from "./logic/client.js";
-import { renderEnding } from "./components/ending/ending.js";
+// import { renderEnding } from "./components/ending/ending.js";
 // import { renderQuizNav, renderQuizHeader, renderQuizQuestion } from "./components/quiz/renderQuiz.js";
 // import { renderWrongAnswer } from "./components/wrongAnswer/renderWrongAnswer.js";
 
 export const ws = establishWebsocket();
 const wrapper = document.getElementById("wrapper");
 
-// renderStart(wrapper);
+renderStart(wrapper);
 // renderQuizNav(wrapper);
 // renderRanking(wrapper, false);
 // renderQuizHeader(wrapper);
@@ -17,5 +17,5 @@ const wrapper = document.getElementById("wrapper");
 //renderWrongAnswer(wrapper);
 // renderFinalScore(wrapper);
 
-renderEnding(wrapper);
+// renderEnding(wrapper);
 
