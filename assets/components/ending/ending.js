3645d1fd3823d0871d043a4afebdd49eaa9694e3
theoskogsberg <theo.skogsberg02@gmail.com>
2024-12-23 16:15:45 +0100

    ending page done

--- assets/components/ending/ending.js
+++ assets/components/ending/ending.js
@@ -1,14 +1,48 @@
 
-function renderEnding (parentElement) {
+export function renderEnding (parentElement) {
+    parentElement.innerHTML = "";
+
     let container = document.createElement("div");
-    container.classList.add("container");
+    container.classList.add("endingContainer");
 
     container.innerHTML = `
-        <h2 class= "endingTitle">Want to Play Again?</h2>
-        <h4 class= "h4-bold">Vote for the next quiz to play:</h4>
-        <p class= "voteInfoText">Click to vote!</p>
-
-        
-
+        <h2 class="endingTitle">Want to Play Again?</h2>
+        <h4 class="h4-bold">Our Most Popular Quizzes:</h4>
+        <p class="infoText">Wait for the moderator to start a new quiz!</p>
+        <div class="popularContainer"></div>
+        <div class="divider"></div>
+        <div class="homeButtonWrap">
+            <p>Home</p>
+        </div>
     `;
+    parentElement.appendChild(container);
+
+    // Rendera rekommenderade quiz
+    let quizContainer = document.querySelector(".popularContainer");
+    let suggestedGenres = ["Country", "Hiphop", "Jazz", "Pop"];
+    renderGenresEnding(quizContainer, suggestedGenres);
+    
+
+    // Hemknapp 
+    let homeButton = document.querySelector(".homeButtonWrap");
+    homeButton.addEventListener("click", () => {
+        // Funktioner för hemknappen
+    });
 };
+
+function renderGenresEnding(parentElement, allGenres) {
+    for (let genre of allGenres) {
+        const div = document.createElement("div");
+        div.id = genre;
+        div.classList.add("genre");
+        div.innerHTML = `
+            <div class="genreImg">
+                <img src="/static/media/images/${genre}.jpg">
+            </div>
+            <div class="genreText">
+                <p class="p-bold">${genre}</p>
+            </div>
+        `;
+        parentElement.appendChild(div);
+    }
+}
--- assets/index.js
+++ assets/index.js
@@ -1,12 +1,13 @@
-import { renderStart } from "./components/home/renderHome.js";
+// import { renderStart } from "./components/home/renderHome.js";
 import { establishWebsocket } from "./logic/client.js";
+import { renderEnding } from "./components/ending/ending.js";
 // import { renderQuizNav, renderQuizHeader, renderQuizQuestion } from "./components/quiz/renderQuiz.js";
 // import { renderWrongAnswer } from "./components/wrongAnswer/renderWrongAnswer.js";
 
 export const ws = establishWebsocket();
 const wrapper = document.getElementById("wrapper");
 
-renderStart(wrapper);
+// renderStart(wrapper);
 // renderQuizNav(wrapper);
 // renderRanking(wrapper, false);
 // renderQuizHeader(wrapper);
@@ -15,3 +16,6 @@ renderStart(wrapper);
 
 //renderWrongAnswer(wrapper);
 // renderFinalScore(wrapper);
+
+renderEnding(wrapper);
+
